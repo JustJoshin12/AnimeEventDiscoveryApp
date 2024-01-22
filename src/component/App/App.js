8f9ee4846082e114601a3d648b13d176613dfe6e
Joshua Smith <kazenostigma50@gmail.com>
2024-01-22 01:18:23 -0600

    changed file structure

---
+++ src/component/App/App.js
@@ -0,0 +1,17 @@
+import LoginPage from "../LoginPage/LoginPage";
+import SignupPage from "../SignupPage/SignupPage";
+
+
+function App() {
+  return (
+    <>
+    <div className="text-2xl text-red-800 bg-yellow-500">
+      Hello Josh
+    </div>
+    <LoginPage/>
+    <SignupPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/SignupPage/SignupPage.js
@@ -0,0 +1,9 @@
+function SignupPage () {
+    return (
+        <div className="text-black-900">
+            SignupPage
+        </div>
+    )
+}
+
+export default SignupPage
