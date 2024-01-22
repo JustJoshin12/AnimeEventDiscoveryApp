3c0788ad75d5be1e7922901f905bb9fe454098c3
Joshua Smith <kazenostigma50@gmail.com>
2024-01-22 12:31:57 -0600

    added daisyUI dependency

--- src/component/SignupPage/SignupPage.js
+++ src/component/SignupPage/SignupPage.js
@@ -2,6 +2,7 @@ function SignupPage () {
     return (
         <div className="text-black-900">
             SignupPage
+            <button className="btn btn-secondary">Secondary</button>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   plugins: [
+    require('daisyui'),
     require('autoprefixer')
   ],
 }
